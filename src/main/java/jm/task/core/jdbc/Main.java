960b21acc3101c6a9ae9d3726dc6845fa9588653
Anatoliy <rememberw8h8@yandex.ru>
2023-05-06 22:50:20 +0300

    Подключение к Бд

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("22");
+//        Util util = new Util();
+        Util.getJdbcConnection();
         // реализуйте алгоритм здесь
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,39 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    static Scanner scanner = new Scanner(System.in);
+    private static Connection dbConnection;
+    private static String DRIVER = "org.postgresql.Driver";
+    private static String URL = "jdbc:postgresql://localhost:5432/";
+    private static String DATABASE = "dataBaseTest";
+    private static String USERNAME = scanner.nextLine();
+    private static String PASS = scanner.nextLine();
+
+    public static Connection getJdbcConnection() {
+        while (dbConnection == null) {
+
+            try {
+                Class.forName(DRIVER);
+                System.out.println("Драйте успешно загружен");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Не удалось загрузить драйвер");
+                break;
+            }
+            try {
+                dbConnection = DriverManager.getConnection(
+                        URL + DATABASE, USERNAME, PASS);
+                System.out.println("Успешное подлючение к БД");
+            } catch (SQLException e) {
+                System.out.println("Не удалось подключиться к БД");
+                break;
+            }
+        }
+        return dbConnection;
+    }
+
 }
